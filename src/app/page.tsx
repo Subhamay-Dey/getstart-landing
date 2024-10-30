783670ece896da30983356df6216d46031925764
Subhamay-Dey <ncdey1966@gmail.com>
2024-10-30 21:08:04 +0530

    TimeSaver section done ✅.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Herosection from "@/components/Herosection";
 import Pricing from "@/components/Pricing";
+import TimeSaver from "@/components/TimeSaver";
 
 export default function Home() {
   return (
     <div>
       <Herosection/>
+      <TimeSaver/>
       <Pricing/>
     </div>
   );
