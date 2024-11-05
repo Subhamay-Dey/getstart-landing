eb344bb49f6befc616bf4ae33b19687102c7fca9
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-05 17:03:16 +0530

    bug fixed

--- src/app/api/sendZipFile/route.ts
+++ src/app/api/sendZipFile/route.ts
@@ -2,10 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import nodemailer from 'nodemailer';
 import path from 'path';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ message: 'Method not allowed' });
-  }
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+ 
 
   const { email, paymentId } = req.body;
 
