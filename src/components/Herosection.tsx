1a96969ae28bf52bd12d77a23216353864b56fe3
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-06 18:10:25 +0530

    added producthunt link

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -12,6 +12,9 @@ export default function Herosection() {
     <div className='relative min-h-screen w-full flex flex-col justify-center items-center'>
       <Navbar />
       <span className="flex-col flex lg:pt-[170px] pt-[100px]">
+        <div className="flex justify-center items-center">
+          <a href="https://www.producthunt.com/posts/getstart?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-getstart" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=582885&theme=light" alt="Getstart - SaaS&#0032;kit&#0032;with&#0032;built&#0045;in&#0032;tools&#0032;to&#0032;boost&#0032;focus&#0032;and&#0032;productivity | Product Hunt" width="200" height="40" className="py-4"/></a>
+        </div>
         <h1 className='bg-gradient-to-b from-gray-900 via-black to-gray-600 bg-clip-text tracking-tighter text-transparent text-4xl lg:text-6xl font-black text-center dark:from-white dark:via-gray-200 dark:to-gray-400'>
           Build and launch
         </h1>
