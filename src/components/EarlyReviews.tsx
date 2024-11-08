51973c1155c5f8d30ce9526ec2327a6b20341a16
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-08 15:11:15 +0530

    use react-tweet for fast rendering of tweets

--- src/components/EarlyReviews.tsx
+++ src/components/EarlyReviews.tsx
@@ -1,6 +1,61 @@
 'use client'
 import Marquee from './ui/marquee'
 import {motion} from 'framer-motion'
+import { Tweet } from 'react-tweet'
+
+interface Tweet {
+  author: string;
+  handle: string;
+  content: string;
+  date: string;
+  id: string;
+}
+
+const tweets: Tweet[] = [
+  {
+    author: "Martin The M",
+    handle: "Saas_Dude",
+    content: "@Subhraneel55545, seems interesting. what features stood out most? productivity tools certainly help.",
+    date: "November 8, 2024",
+    id: "1854797703546286450"
+  },
+  {
+    author: "Ayush Oswal",
+    handle: "Oswal_ayushh",
+    content: "All the bestt bro!, ui looks amazing🔥🔥",
+    date: "November 5, 2024",
+    id: "1853817244771639413"
+  },
+  {
+    author: "Sheibban Pervez",
+    handle: "Sheibban",
+    content: "@Subhraneel55545, looks like a handy kit for boosting that productivity game. What features stand out the most to you?",
+    date: "November 8, 2024",
+    id: "1854797572721750412"
+  },
+  {
+    author: "Samuel Duc",
+    handle: "samm_duc",
+    content: "@Subhraneel55545, that saas kit looks intriguing. always on the lookout for tools to enhance productivity. have you tried it out yet?",
+    date: "November 8, 2024",
+    id: "1854797185352602060"
+  },
+  {
+    author: "Martin The M",
+    handle: "Saas_Dude",
+    content: "@Subhraneel55545, impressive milestone. wishing you a successful launch and growth.",
+    date: "November 6, 2024",
+    id: "1854151499443552465"
+  },
+  {
+    author: "Samuel Duc",
+    handle: "samm_duc",
+    content: "@Subhraneel55545, that sounds like a smart solution. streamlining those repetitive tasks can really boost creativity. what features are you most excited about?",
+    date: "November 1, 2024",
+    id: "1854797703546286450"
+  }
+];
+
 export default function EarlyReviews() {
 
 
@@ -16,17 +71,11 @@ export default function EarlyReviews() {
         <Marquee className="w-full">
             <div className="flex gap-4 px-4">
                 <div className='flex gap-4 mt-12'>
-                    <blockquote className="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr"><a href="https://twitter.com/Subhraneel55545?ref_src=twsrc%5Etfw">@Subhraneel55545</a>, seems interesting. what features stood out most? productivity tools certainly help.</p>&mdash; Martin The M (@Saas_Dude) <a href="https://twitter.com/Saas_Dude/status/1854797703546286450?ref_src=twsrc%5Etfw">November 8, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
-                    <blockquote className="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr">All the bestt bro!, ui looks amazing🔥🔥</p>&mdash; Ayush Oswal (@Oswal_ayushh) <a href="https://twitter.com/Oswal_ayushh/status/1853817244771639413?ref_src=twsrc%5Etfw">November 5, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
-                    <blockquote className="twitter-tweet" data-conversation="none" data-theme="light"><p lang="en" dir="ltr"><a href="https://twitter.com/Subhraneel55545?ref_src=twsrc%5Etfw">@Subhraneel55545</a>, looks like a handy kit for boosting that productivity game. What features stand out the most to you?</p>&mdash; Sheibban Pervez (@Sheibban) <a href="https://twitter.com/Sheibban/status/1854797572721750412?ref_src=twsrc%5Etfw">November 8, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
-                    <blockquote className="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr"><a href="https://twitter.com/Subhraneel55545?ref_src=twsrc%5Etfw">@Subhraneel55545</a>, that saas kit looks intriguing. always on the lookout for tools to enhance productivity. have you tried it out yet?</p>&mdash; Samuel Duc (@samm_duc) <a href="https://twitter.com/samm_duc/status/1854797185352602060?ref_src=twsrc%5Etfw">November 8, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
-                    <blockquote className="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr"><a href="https://twitter.com/Subhraneel55545?ref_src=twsrc%5Etfw">@Subhraneel55545</a>, impressive milestone. wishing you a successful launch and growth.</p>&mdash; Martin The M (@Saas_Dude) <a href="https://twitter.com/Saas_Dude/status/1854151499443552465?ref_src=twsrc%5Etfw">November 6, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
-
-                    <blockquote className="twitter-tweet" data-conversation="none"><p lang="en" dir="ltr"><a href="https://twitter.com/Subhraneel55545?ref_src=twsrc%5Etfw">@Subhraneel55545</a>, that sounds like a smart solution. streamlining those repetitive tasks can really boost creativity. what features are you most excited about?</p>&mdash; Samuel Duc (@samm_duc) <a href="https://twitter.com/samm_duc/status/1852313080069292065?ref_src=twsrc%5Etfw">November 1, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                    {tweets.map((tweet, index) => (
+                      <div key={index} className="min-w-[350px]">
+                        <Tweet id={tweet.id} />
+                      </div>
+                    ))}
                 </div>
             </div>
         </Marquee>
