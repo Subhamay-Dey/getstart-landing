542cd3e0fab3c9b873f38ff2e33d65bab1ad6b6e
subhraneel2005 <digitalsubhraneel@gmail.com>
2024-11-09 19:57:38 +0530

    dd

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -110,7 +110,7 @@ export default function Navbar() {
                 </ul>
               </NavigationMenuContent>
             </NavigationMenuItem>
-            <Link href={'https://deadpan-eclipse-267.notion.site/Getstart-Documentation-12abb54875e6808ab04bef2334a05b04?pvs=4'} target="_blank">
+            <Link href={'https://deadpan-eclipse-267.notion.site/Getstart-Documentation-12abb54875e6808ab04bef2334a05b04'} target="_blank">
               <Button variant={'ghost'}>
                 Documentation
               </Button>
@@ -166,7 +166,7 @@ export default function Navbar() {
                     </ul>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
-                <Link href={'https://deadpan-eclipse-267.notion.site/Getstart-Documentation-12abb54875e6808ab04bef2334a05b04?pvs=4'} target="_blank">
+                <Link href={'https://deadpan-eclipse-267.notion.site/Getstart-Documentation-12abb54875e6808ab04bef2334a05b04'} target="_blank">
                   <Button variant={'ghost'}>
                     Documentation
                   </Button>
